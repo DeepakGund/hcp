6665836ec5a9f4a2be9f3273e6402b83dcf14161
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-11-04 12:14:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,24 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "ec2" {
-  count          = 4
-  ami            = "ami-022b9b4e935404526"
-  instance_type  = "t2.medium"
-  key_name       = "us-west-02"  # Corrected the key name attribute
-  vpc_security_group_ids = ["sg-0d890a3779134bcbe"]  # Corrected the security group attribute
-
-  tags = {
-    Name = var.instance_name[count.index]  # Fixed the variable reference and capitalized "Name" key
-  }
+variable "vpc_id" {
+  default = "vpc-0e82420fb68e6585c"
 }
 
 variable "instance_name" {
   default = ["jenkins.server", "tomcat", "3", "4"]
+}
+
+resource "aws_instance" "ec2" {
+  count           = 4
+  ami             = "ami-022b9b4e935404526"
+  instance_type   = "t2.medium"
+  key_name        = "us-west-02"
+  vpc_security_group_ids = ["sg-0d890a3779134bcbe"]
+  
+  subnet_id       = var.vpc_id  # Adding the VPC ID to associate the instances with the specified VPC
+
+  tags = {
+    Name = var.instance_name[count.index]
+  }
 }
