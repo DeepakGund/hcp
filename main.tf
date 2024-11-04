7fbc203f492aaad60bd7af671ffd805e8bde91ce
DeepakGund <126459378+DeepakGund@users.noreply.github.com>
2024-11-04 13:37:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "vpc_id" {
 }
 
 variable "instance_name" {
-  default = ["jenkins.server", "tomcat", "3", "4"]
+  default = ["jenkins.server", "Tomcat", "Nesus", "4"]
 }
 
 resource "aws_instance" "ec2" {
@@ -16,7 +16,12 @@ resource "aws_instance" "ec2" {
   instance_type          = "t2.medium"
   key_name               = "us-west-02"
   vpc_security_group_ids = ["sg-0d890a3779134bcbe"]
-  subnet_id              = "subnet-0b1be3912a15fbacc"  # Adding the specified subnet ID
+  subnet_id              = "subnet-0b1be3912a15fbacc"
+
+  root_block_device {
+    volume_size = 20  # Set the root volume size to 20 GB
+    volume_type = "gp2"  # Optional: Choose volume type (gp2, gp3, io1, etc.)
+  }
 
   tags = {
     Name = var.instance_name[count.index]
